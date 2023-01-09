dce9786bd8a779e7720392466d2f71419f26995a
Harinder Singh <harmiglany@yahoo.com>
2023-01-08 19:31:30 -0800

    commit tree 92453b83e37992a8dbfcfde7c7005ae6387370c9

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 # pp [{ id: 1, hello: "World" }, 
 #     { id: 2, hello: "Ruby" }, 
 #     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+#     { id: 4, hello: "Learner" }]puts
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+puts "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
@@ -14,10 +16,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
